58371e73050b03217f1e33f98cd95deeb6ba0253
PRONOY <pronoybanik82@gmail.com>
2022-11-25 15:02:47 +0600

    booking post complite

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.POST|| 5000;
+const port = process.env.POST || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -10,7 +11,52 @@ app.use(express.json());
 
 
 
-app.get('/', (req,res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ru2hz6y.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const carProductsCollection = client.db('carSite').collection('carCollection')
+        const carBookingCollection = client.db('carSite').collection('booking')
+
+
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const result = await carProductsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await carProductsCollection.findOne(query)
+            res.send(service)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const user = req.body;
+            const result = await carBookingCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const booking = await carBookingCollection.find(query).toArray();
+            res.send(booking);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
     res.send('cars server site is running')
 });
 
